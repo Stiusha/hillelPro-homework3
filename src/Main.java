359c1263e5b161bbbbff08ba8041f36841b0b2d3
stiusha <stiusha0110@gmail.com>
2026-04-10 13:45:28 +0300

    HM3 add Fahrenheit to Celsius conv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
+    private static final double FAHRENHEIT_MULTIPLIER_TO_CELSIUS = 5.0 / 9;
+    private static final int FAHRENHEIT_OFFSET = 32;
+
     public static void main(String[] args) {
         System.out.println("App for temperatures converting.");
+
+        double fahrenheit = 451.0;
+        double celsiusByFahrenheit = convFahrenheitToCelsius(fahrenheit);
+        System.out.printf("Fahrenheit: %.2f%n", fahrenheit);
+        System.out.printf("Celsius: %.2f%n", celsiusByFahrenheit);
+    }
+
+    private static double convFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - FAHRENHEIT_OFFSET) * FAHRENHEIT_MULTIPLIER_TO_CELSIUS;
     }
 }
